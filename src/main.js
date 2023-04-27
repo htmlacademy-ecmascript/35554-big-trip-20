7c1cbbdf658f758077f18057ff20c1c9934030a2
Nanammii <kaddia@gmail.com>
2023-04-27 13:50:16 +0700

    creater presenter

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import TripInfoView from './view/trip-info-view';
 import FilterView from './view/filter-view';
-import {render} from './render';
+import {render, RenderPosition} from './render';
+import TripPresenter from './presenter/trip-presenter';
 
 const siteTripMainElement = document.querySelector('.trip-main');
 const siteFilterElement = siteTripMainElement.querySelector('.trip-controls__filters');
+const tripEventsElement = document.querySelector('.trip-events');
+const tripPresenter = new TripPresenter({tripContainer: tripEventsElement});
 
-render(new TripInfoView(), siteTripMainElement);
+render(new TripInfoView(), siteTripMainElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), siteFilterElement);
+
+tripPresenter.init();
 
