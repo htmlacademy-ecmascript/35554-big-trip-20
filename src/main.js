3862eb276b6f098418cc152d3f89b9be4b04e45d
Nanammii <kaddia@gmail.com>
2023-06-06 00:05:47 +0700

    added filter-model.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import TripPresenter from './presenter/trip-presenter';
 import FilterPresenter from './presenter/filter-presenter';
 import EventsModel from './model/events-model';
 import {generateFilter} from './mock/filters';
+import FilterModel from './model/filter-model';
 
 const siteTripMainElement = document.querySelector('.trip-main');
 const tripEventsElement = document.querySelector('.trip-events');
 const eventsModel = new EventsModel();
+const filterModel = new FilterModel();
 const filters = generateFilter(eventsModel.events);
 
 const tripPresenter = new TripPresenter({
